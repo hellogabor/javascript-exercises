82f39d5f32f856aa5959d898965475f897b7c3f5
Gabor Nagy <11444120+hellogabor@users.noreply.github.com>
2023-10-18 15:57:28 +0200

    Complete #3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -2,10 +2,23 @@ const repeatString = function(string, num) {
     if (num < 0) {
         return 'ERROR';
     } else {
+        let rptString = '';
+        for (let i = 0; i < num; i++) {
+        rptString = rptString + string;
+         }
+        return rptString;
+    }
+}
+
+
+/* const repeatString = function(string, num) {
+    if (num < 0) {
+        return 'ERROR';
+    } else {
         return string.repeat(num);
     }
 };
-console.log(repeatString('hey',3))
+console.log(repeatString('hey',3)) */
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let arrayString = string.split('');
+    let i = arrayString.length - 1;
+    let reversedString = '';
+   
+    while (i >= 0) {
+        reversedString += (arrayString[i]);
+        i--;
+    }
+    return reversedString;
 };
+
+
+/* return console.log(arrayLength);
+console.log(reverseString(string)) */
 
 // Do not edit below this line
 module.exports = reverseString;
