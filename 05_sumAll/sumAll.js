0d7b20eaebcbbad6e8523f7cdac93d56998f1093
Gabor Nagy <11444120+hellogabor@users.noreply.github.com>
2023-10-18 18:31:13 +0200

    Complete #5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,36 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a > b) {
+        let c = a;
+        a = b;
+        b = c;
+    }
 
+    if (a < 0 || b < 0) {
+        return 'ERROR';
+    }
+
+    if ((typeof a !== 'number') || (typeof b !== 'number')) {
+        return 'ERROR';
+    }
+
+    let allNumbers = [];
+    let i = a;
+    while (i <= b) {
+        allNumbers.push(i);
+        i++;
+    }
+
+    let sum = 0;
+        allNumbers.forEach((number) => {
+        sum += number;
+        });
+    return sum;
 };
+
+/*     const sum = allNumbers.reduce((accumulator, currentValue) => 
+        accumulator + currentValue, 0);
+    return sum; */
+
 
 // Do not edit below this line
 module.exports = sumAll;
