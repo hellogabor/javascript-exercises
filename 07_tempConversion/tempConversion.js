7a6a393bd48b4deaab996d519fcee3f1a44111ac
Gabor Nagy <11444120+hellogabor@users.noreply.github.com>
2023-10-18 20:10:01 +0200

    Complete #7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fh) {
+  return +((fh - 32) * (5 / 9)).toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  return +((c * (9 / 5) + 32).toFixed(1));
 };
 
 // Do not edit below this line
