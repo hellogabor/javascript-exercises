053f326e386504bb4d1d75d3edf21234554d5f72
Gabor Nagy <11444120+hellogabor@users.noreply.github.com>
2023-10-18 18:56:06 +0200

    Complete #6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(yearToCheck) {
+    if ((yearToCheck % 4 == 0) && ((yearToCheck % 100 !== 0) || (yearToCheck % 400 == 0))) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
+/* FUNCTION leapYear
+IF leapYear % 4 = 0 AND ( leapYear % 100 = NOT 0 OR leapYear % 400 = 0)
+then return TRUE
+else return FALSE */
 
 // Do not edit below this line
 module.exports = leapYears;
