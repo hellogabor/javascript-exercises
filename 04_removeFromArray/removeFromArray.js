d87a982af456068d46a41aa427a985a745a04058
Gabor Nagy <11444120+hellogabor@users.noreply.github.com>
2023-10-18 17:06:49 +0200

    Complete #4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...elementsToRemove) {
+    let resultsArray = [];
+        for (let i = 0; i < array.length; i++) {
+        if (!elementsToRemove.includes(array[i])) {
+            resultsArray.push(array[i]);
+        }
+    }
+    return resultsArray;
 };
+
+/* function removeFromArray(array, ...elementsToRemove)
+    Initialize a new empty array, resultArray
+    For each element in array
+        If element is not in elementsToRemove
+            Add element to resultArray
+    Return resultArray
+ */
 
 // Do not edit below this line
 module.exports = removeFromArray;
